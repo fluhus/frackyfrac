555fc1859a7a82cbc0d645c040fe79b807b0e30b
Amit Lavon <amit.lavon@weizmann.ac.il>
2023-05-28 11:12:04 +0300

    Add run.sh to testdata

---
+++ testdata/run.sh
@@ -0,0 +1,20 @@
+# Runs frcfrc on the test data and checks the results.
+
+frcfrc=../../bin/frcfrc
+
+for f in uwtd1 uwtd2; do
+    $frcfrc -i $f.dense -t $f.tree -o $f.got
+    $frcfrc -s -i $f.sparse -t $f.tree -o $f.s.got
+done 2> /dev/null
+
+for f in wtd; do
+    $frcfrc -w -i $f.dense -t $f.tree -o $f.got
+    $frcfrc -w -s -i $f.sparse -t $f.tree -o $f.s.got
+done 2> /dev/null
+
+for f in uwtd1 uwtd2 wtd; do
+    diff $f.got $f.want
+    diff $f.s.got $f.want
+done
+
+rm *.got
